11dbf7f8467eabb68c688bcbecaa6c518c6afa85
Benderx2 <excectra@gmail.com>
2014-05-26 20:31:19 +0800

    #!bin/bash!!!!!! ftw

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # Better change this to your fucking directory ok?
 # '.' is the current directory so run this script from the source directory.
 export FVM_INCLUDE=. 
-#!/bin/bash   
 set -o verbose
 gcc -c fvm.c -o fvm.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
 gcc -c fvm/error.c -o error.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
