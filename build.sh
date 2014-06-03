6d0a3662799a885d685b58fa509b70a84707c7c4
Bender <sid123@asus-laptop.(none)>
2014-06-03 12:25:36 +0800

    Updated executable and build script

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 # Better change this to your fucking directory ok?
 export FVM_INCLUDE=.
 #!/bin/bash
-# '.' is the current directory so run this script from the source directory.
-export FVM_INCLUDE=. 
+export SDL_INCLUDE=/usr/include/SDL
 set -o verbose
-gcc -c fvm.c -o fvm.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
+gcc -c fvm.c -o fvm.o -I$FVM_INCLUDE -I$SDL_INCLUDE -Wall -Wextra -pedantic -std=gnu99 
 gcc -c fvm/error.c -o error.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
 gcc -c fvm/cpu/mem/memory.c -o mem.o -I$FVM_INCLUDE -Wall -pedantic -std=gnu99
 gcc -c fvm/rom/rom.c -o rom.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
-gcc -c fvm/fcall/fcall.c -o fcall.o -I$FVM_INCLUDE -Wall -Wextra -pedantic -std=gnu99
-gcc -o fvm.out fvm.o error.o mem.o rom.o fcall.o
+gcc -c fvm/fcall/fcall.c -o fcall.o -I$FVM_INCLUDE -I$SDL_INCLUDE -Wall -Wextra -pedantic -std=gnu99
+gcc -c fvm/sdl.c -o sdl.o -I$FVM_INCLUDE -I$SDL_INCLUDE -Wall -Wextra -pedantic -std=gnu99
+gcc -o fvm.out fvm.o error.o mem.o rom.o fcall.o sdl.o -lSDL -lSDL_ttf
 # Remove all object files
 rm *.o
 echo 'FVM Compilation Complete.'
