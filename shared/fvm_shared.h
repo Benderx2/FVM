aa285f94e2433f788ccc74063cc132d6df2a2ef0
Bender <sid123@asus-laptop.(none)>
2014-07-27 17:01:24 +0800

    updated fvm_shared.h

--- shared/fvm_shared.h
+++ shared/fvm_shared.h
@@ -3,4 +3,9 @@
 typedef struct sVM_api {
 	void (*VM_printf)(const char* s, ...);
 } VM_api_t;
+VM_api_t VM_API;
+void lib_init(void* vm_printf_ptr)
+{
+	VM_API.VM_printf = vm_printf_ptr;
+}
 #endif
--- shared/hello.c
+++ shared/hello.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "fvm_shared.h"
-VM_api_t VM_API;
-void lib_init(void* vm_printf_ptr)
-{
-	VM_API.VM_printf = vm_printf_ptr;
-}
 uint32_t hello(void* arg)
 {
 	VM_API.VM_printf("hello from shared object\n");
