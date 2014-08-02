50f2f930a1d48b6d872370030f75817ae114b9ff
Bender <sid123@asus-laptop.(none)>
2014-08-02 11:35:56 +0800

    updated shared example

--- shared/test.c
+++ shared/test.c
@@ -5,12 +5,15 @@
 // We'll call this from our VM!
 uint32_t test_function(void* arg)
 {
+	SDL_Surface* bmp;
 	VM_API.VM_printf("Test function!\n");
 	VM_API.VM_printf("Hey there! pi = %f", 3.14159265f);
 	struct SDL_Rect Temp;
 	VM_API.VM_set_resolution(1024, 768, 32);
 	SDL_Surface* VM_Screen = VM_API.VM_get_surface();
-	SDL_FillRect(VM_Screen, NULL, SDL_MapRGB(VM_Screen->format, 255, 0, 0));
+	bmp = SDL_LoadBMP("./lol.bmp");
+	SDL_BlitSurface(bmp, NULL, VM_Screen, NULL); 	
+	//Update Screen 
 	SDL_Flip(VM_Screen);
 	return 0;
 }
